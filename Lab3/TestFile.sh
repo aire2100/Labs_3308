9fe6ceb96d2c210be1aa0b30b4c4cf4fde43ba59
aire2100 <58457423+aire2100@users.noreply.github.com>
2021-02-10 11:39:05 -0700

    Create TestFile.sh

---
+++ Lab3/TestFile.sh
@@ -0,0 +1,7 @@
+#************
+#!/bin/bash
+#************
+if [ -f $1 ]
+	then echo "$1 Exists."
+	echo -n "Type \n 1 for CountLines \n 2 for CountWords \n 3 for addText \n 4 for copyAndExit \n"
+fi
